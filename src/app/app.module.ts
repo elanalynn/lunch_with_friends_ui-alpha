aff0de39790dedf7b2dacffc6863b3887c8b4a5b
Elana Kopelevich <ekopelevich@gmail.com>
2017-05-19 22:24:17 -0600

    Angular Material setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {
+  MaterialModule,
+  MdMenuModule,
+  MdSidenavModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    MdMenuModule,
+    MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
