2b29802f277768558b6bc3f4dbc36dc1d24779ab
Elana Kopelevich <ekopelevich@gmail.com>
2017-05-21 15:57:16 -0600

    route to spots

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SplashComponent } from './splash/splash.component';
+import { SpotDetailComponent } from './spot-detail/spot-detail.component';
+import { SpotsListComponent } from './spots-list/spots-list.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UsersListComponent } from './users-list/users-list.component';
 
 export const APP_ROUTES: Routes = [
     { path: '', component: SplashComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'spots', component: SpotsListComponent },
+    { path: 'spots/:id', component: SpotDetailComponent },
     { path: 'users', component: UsersListComponent },
     { path: 'users/:id', component: UserDetailComponent },
     { path: '', redirectTo: '/', pathMatch: 'full' },
